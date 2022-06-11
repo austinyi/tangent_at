670ac66faa8fe2b420ac28972793f6e678b6fd8b
Bongsoo Yi <austinyi@naver.com>
2022-06-11 23:52:43 +0900

    standard AT

--- train_standardAT.py
+++ train_standardAT.py
@@ -9,10 +9,10 @@ from torch.autograd import Variable
 from tqdm import tqdm
 from tangent import compute_angle, compute_tangent
 from setup.utils import loaddata, loadmodel, savefile
-from setup.setup_pgd_adaptive import to_var, adv_train, pred_batch, LinfPGDAttack, attack_over_test_data
+from setup.setup_pgd_adaptive import to_var, adv_train, pred_batch, LinfPGDAttack, attack_over_test_data, GA_PGD
 
 
-def get_ep(inputs, epsilon, criterion, method, threshold=0.4, ratio=0.5, precision=3, rou=True):
+def get_ep(inputs, epsilon, criterion, method, exp, threshold=0.4, ratio=0.5, precision=3, rou=True):
     cri_method = criterion + '_' + method
     if cri_method == 'angle_num':
         ep = (1 / (inputs * np.max(1 / inputs))) * epsilon
@@ -42,18 +42,23 @@ def get_ep(inputs, epsilon, criterion, method, threshold=0.4, ratio=0.5, precisi
         rank = np.argsort(np.argsort(inputs)) + 1
         cri = int(inputs.size * ratio)
         ep[rank >= cri] = epsilon
-    elif cri_method == 'angle_rank_square':
+    elif cri_method == 'angle_rank_exp':
         rank = np.argsort(
             np.argsort(1 / inputs)) + 1  # to remove zero, 1/inputs since for angle the smaller the larger the epsilon
-        ep = np.square(rank / inputs.shape[0]) * epsilon
-    elif cri_method == 'tan_rank_square':
+        ep = np.power(rank / inputs.shape[0], exp) * epsilon
+    elif cri_method == 'tan_rank_exp':
         rank = np.argsort(np.argsort(inputs)) + 1
-        ep = np.square(rank / inputs.shape[0]) * epsilon
+        ep = np.power(rank / inputs.shape[0], exp) * epsilon
+    elif cri_method == 'angle_num_exp':
+        ep = np.power((1 / (inputs * np.max(1 / inputs))), exp) * epsilon
+    elif cri_method == 'tan_num_exp':
+        ep = np.power(inputs / np.max(inputs), exp) * epsilon
     else:
         raise Exception("No such criterion method combination")
     if rou:
         ep = np.round(ep, precision)
     return ep
+
 
 
 def trainClassifier(args, model, result_dir, train_loader, test_loader, use_cuda=True):
@@ -93,6 +98,7 @@ def trainClassifier(args, model, result_dir, train_loader, test_loader, use_cuda
     return model
 
 
+
 def testClassifier(test_loader, model, use_cuda=True, batch_size=100):
     model.eval()
     correct_cnt = 0
@@ -120,6 +126,18 @@ def testattack(classifier, test_loader, args, use_cuda=True):
     acc = attack_over_test_data(classifier, adversary, param, test_loader, use_cuda=use_cuda)
     return acc
 
+def eval_robust(model, test_loader, perturb_steps, epsilon, step_size, loss_fn, category, random):
+    model.eval()
+    correct = 0
+    with torch.enable_grad():
+        for batch_idx, (data, target) in enumerate(test_loader):
+            data, target = data.cuda(), target.cuda()
+            x_adv, _ = GA_PGD(model,data,target,epsilon,step_size,perturb_steps,loss_fn,category,rand_init=random)
+            output = model(x_adv)
+            pred = output.max(1, keepdim=True)[1]
+            correct += pred.eq(target.view_as(pred)).sum().item()
+    test_accuracy = correct / len(test_loader.dataset)
+    return test_accuracy
 
 def main(args):
     use_cuda = torch.cuda.is_available()
@@ -134,13 +152,15 @@ def main(args):
     model = trainClassifier(args, model, result_dir, train_loader, test_loader, use_cuda=use_cuda)
     testClassifier(test_loader, model, use_cuda=use_cuda, batch_size=args['batch_size'])
     testattack(model, test_loader, args, use_cuda=use_cuda)
-
+    test_pgd20_acc = eval_robust(model, test_loader, perturb_steps=20, epsilon=0.031, step_size=0.031 / 4, loss_fn="cent",
+                category="Madry", random=True)
+    print(test_pgd20_acc)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Training defense models')
     parser.add_argument("-d", '--dataset', choices=["mnist", "cifar10", "stl10", "tiny"], default="cifar10")
-    parser.add_argument("-m", '--model', choices=["vgg16", "wrn"], default="vgg16")
-    parser.add_argument("-n", "--num_epoch", type=int, default=100)
+    parser.add_argument("-m", '--model', choices=["vgg16", "wrn"], default="wrn")
+    parser.add_argument("-n", "--num_epoch", type=int, default=120)
     parser.add_argument("-f", "--file_name", default="cifar10_adapt")
     #parser.add_argument("-l", "--lr", type=float, default=1e-3)
     parser.add_argument('--lr-schedule', default='piecewise',
@@ -149,7 +169,7 @@ if __name__ == "__main__":
     parser.add_argument('--lr-one-drop', default=0.01, type=float)
     parser.add_argument('--lr-drop-epoch', default=100, type=int)
     parser.add_argument("--criterion", default='angle', choices=['angle', 'tan'])
-    parser.add_argument("--method", default='num', choices=['num', 'rank','skip','rank_binary','rank_square'])
+    parser.add_argument("--method", default='num', choices=['num', 'rank','skip','rank_binary','rank_exp','num_exp'])
     parser.add_argument("--round", action="store_true", default=False, help='if true, round epsilon vector')
     parser.add_argument("--precision", type=int, default=4, help='precision of rounding the epsilon vector')
     parser.add_argument("--init", default=None, help='initial the model with pre-trained one')
@@ -167,6 +187,7 @@ if __name__ == "__main__":
     parser.add_argument('--threshold', type=float, default=0.4, help='adaptive train threshold')
     parser.add_argument('--train_ratio', type=float, default=0.5, help='adaptive train ratio')
     parser.add_argument('--train_epsilon', type=float, default=0.031, help='adaptive train ratio')
+    parser.add_argument('--exp', type=float, default=2, help='criterion exponent')
     args = vars(parser.parse_args())
     args['file_name'] = args['file_name'] + '_' + args['criterion'] + '_' + args['method']
     if args['dataset'] == 'mnist':

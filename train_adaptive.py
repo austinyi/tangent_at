9f636fb0b75b156175bc5e6b093c006b0a050f0a
Bongsoo Yi <austinyi@naver.com>
2022-06-15 14:53:00 +0900

    standard AT

--- train_adaptive.py
+++ train_adaptive.py
@@ -75,6 +75,9 @@ def trainClassifier(args, model, result_dir, train_loader, test_loader, use_cuda
             x, target = to_var(x), to_var(target)
             if args['clean']:
                 x_adv = x
+            elif args['standard']:
+                target_pred = pred_batch(x, model)
+                x_adv = adv_train(x, target_pred, model, train_criterion, adversary)
             else:
                 target_pred = pred_batch(x, model)
                 x_adv_init = adv_train(x, target_pred, model, train_criterion, adversary)
@@ -190,6 +193,7 @@ if __name__ == "__main__":
     parser.add_argument("--root_data", default=r'/', help='the dir that contains the data folder')
     parser.add_argument("--result_dir", default=r'/data/tangent', help='the working directory that contains AA, AAA')
     parser.add_argument("--clean", action="store_true", default=False, help='if true, clean training')
+    parser.add_argument("--standard", action="store_true", default=False, help='if true, standard adversarial training')
     parser.add_argument("--model_folder", default='./models',
                         help="Path to the folder that contains checkpoint.")
     parser.add_argument("--train_shuffle", action="store_false", default=True,

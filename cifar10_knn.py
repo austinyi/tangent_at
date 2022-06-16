8d2337ed97daa9cd236297bf85a57384a509d57b
Bongsoo Yi <austinyi@naver.com>
2022-06-17 00:54:50 +0900

    knn

--- cifar10_knn.py
+++ cifar10_knn.py
@@ -8,38 +8,6 @@ import pickle
 import platform
 from tqdm import tqdm
 
-def load_pickle(f):
-    version = platform.python_version_tuple()
-    if version[0] == '2':
-        return  pickle.load(f)
-    elif version[0] == '3':
-        return  pickle.load(f, encoding='latin1')
-    raise ValueError("invalid python version: {}".format(version))
-
-def load_CIFAR_batch(filename):
-  """ load single batch of cifar """
-  with open(filename, 'rb') as f:
-    datadict = load_pickle(f)
-    X = datadict['data']
-    Y = datadict['labels']
-    X = X.reshape(10000, 3, 32, 32).transpose(0,2,3,1).astype("float")
-    Y = np.array(Y)
-    return X, Y
-
-def load_CIFAR10(ROOT):
-  """ load all of cifar """
-  xs = []
-  ys = []
-  for b in range(1,6):
-    f = os.path.join(ROOT, 'data_batch_%d' % (b, ))
-    X, Y = load_CIFAR_batch(f)
-    xs.append(X)
-    ys.append(Y)
-  Xtr = np.concatenate(xs)
-  Ytr = np.concatenate(ys)
-  del X, Y
-  Xte, Yte = load_CIFAR_batch(os.path.join(ROOT, 'test_batch'))
-  return Xtr, Ytr, Xte, Yte
 
 def load_CIFAR101(train_loader):
     i = 0
@@ -52,7 +20,6 @@ def load_CIFAR101(train_loader):
             X_train = torch.cat([X_train, x], axis=0)
             idx_train = torch.cat([idx_train, idx], axis=0)
     return X_train, idx_train
-
 
 def main(args):
     use_cuda = torch.cuda.is_available()
@@ -84,15 +51,15 @@ def main(args):
     pickle.dump(knn, open(filename, 'wb'))
 
     # load the model from disk
-    knn = pickle.load(open(filename, 'rb'))
+    #knn = pickle.load(open(filename, 'rb'))
     #print(X_test[[0],:].shape)
     #print(knn.predict(X_test[[0],:]))
-    predict = knn.predict(X_train)
-    print(predict) # [47189 42769 21299 ... 13253 17940 29497]
-    print(y_train)
+    #predict = knn.predict(X_train)
+    #print(predict) # [47189 42769 21299 ... 13253 17940 29497]
+    #print(y_train)
     #print(knn.predict(X_train))
 
-    np.save('./models/knn_X_test.npy', predict)
+    #np.save('./models/knn_X_test.npy', predict)
 
 
 if __name__ == "__main__":

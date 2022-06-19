e1698d64c73c6a5ed822f121ed01789cfa163ba5
Bongsoo Yi <austinyi@naver.com>
2022-06-19 00:12:13 -0400

    edit comment

--- cifar10_knn.py
+++ cifar10_knn.py
@@ -12,6 +12,7 @@ from tqdm import tqdm
 def load_CIFAR10(train_loader):
     i = 0
     for idx, x, target in tqdm(train_loader):
+        print(idx)
         if i == 0:
             X_train = x
             idx_train = idx

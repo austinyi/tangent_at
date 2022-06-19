b17b9a725220245acfa61d4b02037ea38eda3cf1
Bongsoo Yi <austinyi@naver.com>
2022-06-19 00:11:51 -0400

    edit comment

--- compute_A.py
+++ compute_A.py
@@ -32,6 +32,7 @@ def load_autoencoder(args):
 
 def saveA_AA_AAA(args, autoencoder, train_loader, result_dir):
     for idx, x, target in tqdm(train_loader):
+        print(idx)
         x, target = to_var(x), to_var(target)
         save_AA(args, autoencoder, x, result_dir, idx, k=args['k'])
         #save_AAA(args, autoencoder, x, result_dir, idx, k=10)

1db9c29ee41936a74be067da492256403fc4f98a
Bongsoo Yi <austinyi@naver.com>
2022-07-11 21:04:11 -0400

    reweight

--- train_reweight.py
+++ train_reweight.py
@@ -76,6 +76,7 @@ def reweightedLoss(logs, targets, ep):
     out = torch.zeros_like(targets, dtype=torch.float)
     for i in range(len(targets)):
         out[i] = logs[i][targets[i]]*ep[i]/0.031
+    print(out.sum())
     return -out.sum()/len(out)
 
 
